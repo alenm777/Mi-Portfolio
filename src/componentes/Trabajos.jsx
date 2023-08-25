1cbce5be757341f1781f0c30154d7e35051048b6
Alén <alenm.1717@gmail.com>
2023-08-25 19:50:59 -0300

    ya falta menos

--- src/componentes/Trabajos.jsx
+++ src/componentes/Trabajos.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import workImg from '../assests/crypto-x.jpeg';
-import RealEst from '../assests/Sport-Planet.jpg';
+import Crypto from '../assests/crypto-x.jpeg';
+import SportP from '../assests/Sport-Planet.jpg';
  
 
 const Trabajos = () => {
+
+
+const handleWeb = () => {
+        window.open("https://cryptoxapp.netlify.app/")
+    };
+    
+    const handleCodigo = () => {
+        window.open("https://github.com/alenm777/cryptoapp")
+    };
+
+    
+    const handleWebDos = () => {
+        window.open("https://sport-planet.vercel.app/")
+    };
+
+  const handleCodigoDos = () => {
+        window.open("https://github.com/alenm777/SportPlanet/")
+    };
+
   return (
     <div name='trabajos' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
         <div className='max-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -17,7 +36,7 @@ const Trabajos = () => {
 
     {/* Grid Item */}  
     <div
-    style={{backgroundImage: `url(${workImg})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
+    style={{backgroundImage: `url(${Crypto})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
     >
 
         { /* Hover Effect */}
@@ -26,39 +45,35 @@ const Trabajos = () => {
 Crypto X App 
                </span>
                 <div className='pt-8 text-center '>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Sitio Web</button>
-</a>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Codigo</button>
-</a>
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWeb} >Sitio Web</button>
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigo} >Codigo</button>
                 </div>
             </div>
         </div>
-        <div
-    style={{backgroundImage: `url(${RealEst})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
-    >
 
+        <div
+    style={{backgroundImage: `url(${SportP})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
+    >
         { /* Hover Effect */}
         <div className='opacity-0 group-hover:opacity-100'>
                <span className='text-2xl font-bold text-white tracking-wider'>
    Sport-Planet
                </span>
                 <div className='pt-8 text-center '>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Sitio Web
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWebDos}>Sitio Web
     </button>
-</a>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Codigo
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigoDos}>Codigo
     </button>
-</a>
+
                 </div>
             </div>
         </div>
           {/* Grid Item */}  
     <div
-    style={{backgroundImage: `url(${workImg})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
+    style={{backgroundImage: `url(${Crypto})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
     >
 
         { /* Hover Effect */}
@@ -67,17 +82,17 @@ Crypto X App
 Crypto X App 
                </span>
                 <div className='pt-8 text-center '>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Sitio Web</button>
-</a>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Codigo</button>
-</a>
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWeb}>Sitio Web</button>
+
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigo}>Codigo</button>
+
                 </div>
             </div>
         </div>
         <div
-    style={{backgroundImage: `url(${RealEst})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
+    style={{backgroundImage: `url(${SportP})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
     >
 
         { /* Hover Effect */}
@@ -86,20 +101,20 @@ Crypto X App
    Sport-Planet
                </span>
                 <div className='pt-8 text-center '>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Sitio Web
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWebDos}>Sitio Web
     </button>
-</a>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Codigo
+
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigoDos}>Codigo
     </button>
-</a>
+
                 </div>
             </div>
         </div>
           {/* Grid Item */}  
     <div
-    style={{backgroundImage: `url(${workImg})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
+    style={{backgroundImage: `url(${Crypto})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
     >
 
         { /* Hover Effect */}
@@ -108,17 +123,17 @@ Crypto X App
 Crypto X App 
                </span>
                 <div className='pt-8 text-center '>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Sitio Web</button>
-</a>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Codigo</button>
-</a>
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWeb}>Sitio Web</button>
+
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigo}>Codigo</button>
+
                 </div>
             </div>
         </div>
         <div
-    style={{backgroundImage: `url(${RealEst})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
+    style={{backgroundImage: `url(${SportP})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
     >
 
         { /* Hover Effect */}
@@ -127,14 +142,14 @@ Crypto X App
    Sport-Planet
                </span>
                 <div className='pt-8 text-center '>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Sitio Web
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWebDos}>Sitio Web
     </button>
-</a>
-<a href="/">
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Codigo
+
+
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigoDos}>Codigo
     </button>
-</a>
+
                 </div>
             </div>
         </div>
