1cfcddbba1c626a725cd54dab602b9c1fd87e226
Alén <alenm.1717@gmail.com>
2023-08-28 10:59:24 -0300

    volviste guacho

--- src/componentes/Trabajos.jsx
+++ src/componentes/Trabajos.jsx
@@ -25,7 +25,7 @@ const handleWeb = () => {
 
   return (
     <div name='trabajos' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-        <div className='max-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full  '>
+        <div className='max-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full items-center '>
             <div className='pb-8 '>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-600'>Trabajos</p>
                 <p className=' py-6'>// Vea algunos de mis trabajos</p>
