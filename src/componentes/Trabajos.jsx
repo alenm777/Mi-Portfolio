9af646582ee9668d72772faa049ed6654f4021f3
Alén <alenm.1717@gmail.com>
2023-09-03 18:16:50 -0300

    seguimos sumando

--- src/componentes/Trabajos.jsx
+++ src/componentes/Trabajos.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Crypto from '../assests/crypto-x.jpeg';
 import SportP from '../assests/Sport-Planet.jpg';
- 
+import Game from "../assests/Gameratings.jpg";
+
 
 const Trabajos = () => {
 
@@ -22,6 +23,15 @@ const handleWeb = () => {
   const handleCodigoDos = () => {
         window.open("https://github.com/alenm777/SportPlanet/")
     };
+
+    const handleWebTres = () => {
+        window.open("https://gameratings.vercel.app/")
+    };
+
+const handleCodigoTres = () => {
+        window.open("https://github.com/alenm777/GameRatings")
+    };
+
 
   return (
     <div name='trabajos' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
@@ -67,22 +77,19 @@ Crypto X App
         </div>
  
     <div
-    style={{backgroundImage: `url(${Crypto})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
+    style={{backgroundImage: `url(${Game})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
     >
         <div className='opacity-0 group-hover:opacity-100'>
                <span className='text-2xl font-bold text-white tracking-wider'>
-Crypto X App 
+Gameratings
                </span>
                 <div className='pt-8 text-center '>
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWeb}>Sitio Web</button>
-    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigo}>Codigo</button>
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleWebTres}>Sitio Web</button>
+    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' onClick={handleCodigoTres}>Codigo</button>
                 </div>
             </div>
         </div>
-        <div
-    style={{backgroundImage: `url(${SportP})`}}  className='shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div'
-    >
-        </div>
+      
     </div>
 </div>
 </div>
